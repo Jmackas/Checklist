5a9732fec755c377ffcef1a07c2c33997edc7c08
James Mackay <jamesrossmackay@gmail.com>
2019-05-10 20:38:56 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,30 @@
+let addTaskButton = document.getElementById("add-task");
+let newTaskInput = document.getElementById("task-input");
+let todoListContainer = document.getElementById("todo-list");
+
+let templateElement = document.getElementById("list-item-template");
+let template = templateElement.innerHTML;
+
+function onAddTaskClicked(event) {
+
+    let taskName = newTaskInput.value;
+    newTaskInput.value = "";
+    let taskHTML = template.replace("<!-- TASK_NAME -->", taskName);
+    todoListContainer.insertAdjacentHTML('beforeend', taskHTML);
+}
+
+function onTodolistClicked(event) {
+    let targetElement = event.target;
+    while (!targetElement.classList.contains("task")) {
+        targetElement = targetElement.parentElement;
+    }
+
+    let checkbox = targetElement.querySelector(".checkbox");
+    if (checkbox.checked) {
+        targetElement.classList.add("completed");
+    } else {
+        targetElement.classList.remove("completed");
+    }
+}
+addTaskButton.addEventListener('click', onAddTaskClicked);
+todoListContainer.addEventListener('click', onTodolistClicked);
